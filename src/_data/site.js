09fdb46514657b3a21f897ce755847a5f7a416de
echoreader <tom.bisnis@gmail.com>
2025-11-21 13:53:57 +0700

    Update Url

--- src/_data/site.js
+++ src/_data/site.js
@@ -1,6 +1,6 @@
 module.exports = {
-  url: process.env.URL 
-    || process.env.SITE_URL 
+  url: process.env.SITE_URL 
+    || process.env.URL 
     || process.env.CF_PAGES_URL 
     || "http://localhost:8080"
 };
